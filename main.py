b32cf0596a353a68e557d40da9651ed705c75541
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-01-08 18:39:49 -0400

    extrayendo datos por separados

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def main():
     excel_df = pd.read_excel(NOTAS_ALUMNOS,
                              sheet_name='Notas')
     # print(excel_df)
-    for index, row in excel_df.iterrows():
-        print(f'ID: {index} Nombre: {row['NOMBRE']}')
+    # for index, row in excel_df.iterrows():
+    #     print(f'ID: {index} Nombre: {row['NOMBRE']}')
+
+    asig_lista = list(excel_df['ASIGNATURA'].drop_duplicates())
+    print(f'\nAsignaturas: {asig_lista}')
 
 if __name__ == '__main__':
     main()
