0a8b7148fe33becf5a2f01c0a4d034b530996e46
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-01-09 16:03:16 -0400

    extrayendo datos y modiicarlos.

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import pandas as pd
 NOTAS_ALUMNOS = r'SRC/Notas_Alumnos.xlsx'
 # print(NOTAS_ALUMNOS)
 
+dict_asig = {
+    'LENGUA CASTELLANA Y LITERATURA':   'Lengua Castellana y Literatura',
+    'BIOLOGIA':                         'Biología',
+    'GEOGRAFIA E HISTORIA':             'Geografía e Historia',
+    'MATEMATICAS':                      'Matemáticas',
+    'INGLES':                           'Inglés',
+    'EDUCACION FISICA':                 'Educación Física',
+    'ETICA':                            'Ética',
+    'CULTURA CLASICA':                  'Cultura clásica',
+    'MUSICA':                           'Música',
+    'TECNOLOGIA':                       'Tecnología',
+    'EDUCACION PLASTICA':               'Educación Plástica',
+    'FRANCES':                          'Francés',
+}
+
 def main():
     excel_df = pd.read_excel(NOTAS_ALUMNOS,
                              sheet_name='Notas')
@@ -11,8 +26,17 @@ def main():
     # for index, row in excel_df.iterrows():
     #     print(f'ID: {index} Nombre: {row['NOMBRE']}')
 
-    asig_lista = list(excel_df['ASIGNATURA'].drop_duplicates())
-    print(f'\nAsignaturas: {asig_lista}')
+    asig_lista = sorted(
+        list(excel_df['ASIGNATURA'].drop_duplicates())
+    )
+    # print(f'\nAsignaturas: {asig_lista}')
+
+    filtro = []
+    for item in asig_lista:
+        valorID = dict_asig[item]
+        filtro.append(valorID)
+
+    print(f'\nAsignaturas: {filtro}')
 
 if __name__ == '__main__':
     main()
