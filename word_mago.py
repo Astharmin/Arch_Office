1c4dc3477fe8ab913177445b2f1ee4ed70420d7d
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-01-23 19:25:36 -0400

    ajustando filtros de obtencion de datos

--- word_mago.py
+++ word_mago.py
@@ -6,17 +6,20 @@ NOTAS_ALUMNOS = r'SRC/Notas_Alumnos.xlsx'
 PLANTILLA_CURSOS_PATH = r'SRC/Plantilla_Final.docx'
 PATH_OUTPUT = r'.\OUTPUT'
 
-curso = '2021/2022'
+curso = '2021/2025'
 df = pd.read_excel(NOTAS_ALUMNOS, sheet_name='Datos_Alumnos')
 nom_alum_list = sorted(df['NOMBRE'].dropna().unique())
 nom_alumnos = nom_alum_list[0]
+
+filt_datos_alum = df[(df['NOMBRE'] == nom_alumnos)]
+clase = filt_datos_alum.iloc[0]['CLASE']
 
 def main ():
     docs_tpl = DocxTemplate(PLANTILLA_CURSOS_PATH)
     context = {
         'curso' : curso,
         'nombre_alumno': nom_alumnos,
-        'clase': '4-C'
+        'clase': clase
     }
     docs_tpl.render(context)
     docs_tpl.save(PATH_OUTPUT + r'\fichero_word.docx')
