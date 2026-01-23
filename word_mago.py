a7d5838fa1da057d8601adbe71470860bf1a9523
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-01-23 17:27:23 -0400

    ajustando filtros de extraccion de datos

--- word_mago.py
+++ word_mago.py
@@ -6,6 +6,7 @@ NOTAS_ALUMNOS = r'SRC/Notas_Alumnos.xlsx'
 PLANTILLA_CURSOS_PATH = r'SRC/Plantilla_Final.docx'
 PATH_OUTPUT = r'.\OUTPUT'
 
+curso = '2021/2022'
 df = pd.read_excel(NOTAS_ALUMNOS, sheet_name='Datos_Alumnos')
 nom_alum_list = sorted(df['NOMBRE'].dropna().unique())
 nom_alumnos = nom_alum_list[0]
@@ -13,8 +14,8 @@ nom_alumnos = nom_alum_list[0]
 def main ():
     docs_tpl = DocxTemplate(PLANTILLA_CURSOS_PATH)
     context = {
-        'curso' : '2021/2022',
-        'nombre_alumno': 'Jose Torres',
+        'curso' : curso,
+        'nombre_alumno': nom_alumnos,
         'clase': '4-C'
     }
     docs_tpl.render(context)
