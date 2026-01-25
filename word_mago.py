24c61f07ff836000c82ff24f60aa5d62e51d8027
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-01-25 13:46:33 -0400

    agregando modulo de interacion para hacer multiples documentos

--- word_mago.py
+++ word_mago.py
@@ -9,20 +9,25 @@ PATH_OUTPUT = r'.\OUTPUT'
 curso = '2021/2025'
 df = pd.read_excel(NOTAS_ALUMNOS, sheet_name='Datos_Alumnos')
 nom_alum_list = sorted(df['NOMBRE'].dropna().unique())
-nom_alumnos = nom_alum_list[0]
-
-filt_datos_alum = df[(df['NOMBRE'] == nom_alumnos)]
-clase = filt_datos_alum.iloc[0]['CLASE']
+# nom_alumnos = nom_alum_list[0]
 
 def main ():
-    docs_tpl = DocxTemplate(PLANTILLA_CURSOS_PATH)
-    context = {
-        'curso' : curso,
-        'nombre_alumno': nom_alumnos,
-        'clase': clase
-    }
-    docs_tpl.render(context)
-    docs_tpl.save(PATH_OUTPUT + r'\fichero_word.docx')
+    for nom_alumnos in nom_alum_list:
+        filt_datos_alum = df[(df['NOMBRE'] == nom_alumnos)]
+        clase = filt_datos_alum.iloc[0]['CLASE']
+
+        docs_tpl = DocxTemplate(PLANTILLA_CURSOS_PATH)
+        context = {
+            'curso' : curso,
+            'nombre_alumno': nom_alumnos,
+            'clase': clase
+        }
+        docs_tpl.render(context)
+
+        titulo = 'NOTAS_'+nom_alumnos.replace(' ', '_').upper()
+        titulo += '.docx'
+
+        docs_tpl.save(PATH_OUTPUT + r'\fichero_word.docx')
 
 if __name__ == '__main__':
     main()
