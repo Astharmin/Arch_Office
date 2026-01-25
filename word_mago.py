aee4b02e01efaa7f2de7a883b3e09e30c877743f
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2026-01-25 14:02:10 -0400

    agregando estructura de arreglo de formato

--- word_mago.py
+++ word_mago.py
@@ -11,6 +11,21 @@ df = pd.read_excel(NOTAS_ALUMNOS, sheet_name='Datos_Alumnos')
 nom_alum_list = sorted(df['NOMBRE'].dropna().unique())
 # nom_alumnos = nom_alum_list[0]
 
+def elminar_tildes(texto):
+    tildes_dic = {
+        'Á': 'A',
+        'É': 'E',
+        'Í': 'I',
+        'Ó': 'O',
+        'Ú': 'U',
+    }
+    texto_lim = texto
+
+    for key in tildes_dic:
+        texto_lim = texto_lim.replace(key, tildes_dic[key])
+
+    return texto_lim
+
 def main ():
     for nom_alumnos in nom_alum_list:
         filt_datos_alum = df[(df['NOMBRE'] == nom_alumnos)]
@@ -25,6 +40,7 @@ def main ():
         docs_tpl.render(context)
 
         titulo = 'NOTAS_'+nom_alumnos.replace(' ', '_').upper()
+        titulo = elminar_tildes(titulo)
         titulo += '.docx'
 
         docs_tpl.save(PATH_OUTPUT + r'\fichero_word.docx')
